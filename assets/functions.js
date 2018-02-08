8d8844e41e8ac1bb3f6e311cb56a8ead42f184a8
Roland Barrera <rbarrera@rackspace.com>
2018-02-08 09:18:05 -0600

    First commit
    Initial push to to show example of idea.

---
+++ assets/functions.js
@@ -0,0 +1,64 @@
+
+
+function sendStatement(xAPIverbIRI, xAPIverb){
+var lrs;
+
+try {
+    lrs = new TinCan.LRS(
+   {
+            endpoint: "<ADD xAPI ENDPOINT>",
+            username: "<ADD KEY>",
+            password: "<ADD SECRET>",
+            allowFail: false
+        }
+    );
+}
+catch (ex) {
+    console.log("Failed to setup LRS object: " + ex); 
+}
+var statement = new TinCan.Statement(
+    {
+    "actor": {
+        "name": fName.value,
+        "account": {
+                "name": "123456",
+                "homePage": "https://mylearn.rackspace.com",
+        },
+        "mbox": 'mailto:'+emailAddress.value,
+        "objectType": "Agent"
+    },
+    "verb": {
+        "id": xAPIverbIRI,
+        "display": {
+            "en-US": xAPIverb
+        }
+    },
+     "object": {
+
+        "id": document.URL,
+        "definition": {
+            "name": {
+                "en-US": "Test"
+            },
+            "type": "http://adlnet.gov/expapi/activities/media"
+        },
+        "objectType": "Activity"
+    }
+}
+);
+lrs.saveStatement(
+    statement,
+       {
+        callback: function (err, xhr) {
+            if (err !== null) {
+                if (xhr !== null) {
+                    console.log("Failed to save statement: " + xhr.responseText + " (" + xhr.status + ")");
+                    return;
+                }
+                console.log("Failed to save statement: " + err);
+                return;
+            }
+            console.log("Statement saved");
+        }
+    }
+);}
